5dcb69aba32ecdae4d902f5ff3f19858890ebac8
Naveen <naveens@tekion.com>
2026-05-09 23:17:29 +0530

    Two pointer and Sliding window.

--- src/app/DSA/LinkedList_intersection.js
+++ src/app/DSA/LinkedList_intersection.js
@@ -32,3 +32,16 @@ var getIntersectionNode = function (headA, headB) {
   }
   return null;
 };
+
+// Better solution
+var getIntersectionNode = function (headA, headB) {
+  let pA = headA;
+  let pB = headB;
+
+  while (pA != pB) {
+    pA = pA == null ? headB : pA.next;
+    pB = pB == null ? headA : pB.next;
+  }
+
+  return pA;
+};
